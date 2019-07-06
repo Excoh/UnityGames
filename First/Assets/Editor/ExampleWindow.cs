2758f063e0a2d03d1c629c956c32b8069f2abfd3
Excoh <excel.ortega@gmail.com>
2019-07-06 17:46:22 -0500

    Added Basic Player Controller and a Player GO

---
+++ First/Assets/Editor/ExampleWindow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+public class ExampleWindow : EditorWindow
+{
+    [MenuItem("Window/Example")]
+    public static void Init()
+    {
+        GetWindow(typeof(ExampleWindow));
+    }
+
+    private void OnGUI()
+    {
+    }
+}
---
+++ First/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forceAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.MovePosition(transform.position + transform.right * Time.deltaTime * forceAmount);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.MovePosition(transform.position - transform.right * Time.deltaTime * forceAmount);
+        }
+    }
+}
