0a7fbeaef49aa1c91fba44cf54702d99491379cc
Excoh <excel.ortega@gmail.com>
2019-07-06 23:54:51 -0500

    added frustum calculation to set camera follow bounds

--- First/Assets/Scripts/TargetFollow.cs
+++ First/Assets/Scripts/TargetFollow.cs
@@ -15,8 +15,12 @@ public class TargetFollow : MonoBehaviour
     public Camera camera;
     public Color rayColor;
     public GameObject targetToFollow;
+    public Vector3 center;
+    public GameObject playerGO;
     private Vector3 distanceToTarget;
     public float cameraMoveAmount;
+    [Range(1, 100)]
+    public float camBoundSize;
     public float frustumHeight;
     public float frustumWidth;
 
@@ -24,11 +28,13 @@ public class TargetFollow : MonoBehaviour
     {
         camera = GetComponent<Camera>();
     }
+
     // Start is called before the first frame update
     void Start()
     {
         if (targetToFollow != null)
         {
+            center = playerGO.transform.position;
             distanceToTarget = transform.position - targetToFollow.transform.position;
             frustumHeight = 2 * distanceToTarget.magnitude * Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
             frustumWidth = frustumHeight * camera.aspect;
@@ -41,6 +47,25 @@ public class TargetFollow : MonoBehaviour
         if (isDebug)
             Debug.DrawRay(this.transform.position, -distanceToTarget, rayColor);
 
-        this.transform.position = Vector3.MoveTowards(this.transform.position, this.targetToFollow.transform.position + distanceToTarget, cameraMoveAmount);
+        if (playerGO.transform.position.x > (center.x + FrustumWidth/2))
+        {
+            center += new Vector3(playerGO.transform.position.x - (center.x + FrustumWidth/2), 0, 0);
+        }
+
+        if (playerGO.transform.position.x < (center.x - FrustumWidth / 2))
+        {
+            center += new Vector3(playerGO.transform.position.x - (center.x - FrustumWidth / 2), 0, 0);
+        }
+
+        this.transform.position = Vector3.MoveTowards(this.transform.position, center + distanceToTarget, cameraMoveAmount);
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(center, new Vector3(FrustumWidth, FrustumHeight, 5));
+    }
+
+    public float FrustumWidth {get { return frustumWidth * (camBoundSize / 100f); }}
+    public float FrustumHeight { get { return frustumHeight * (camBoundSize / 100f); } }
 }
